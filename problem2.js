0dbfa0d5ea6c2d2283b63d51f0e1d1be50d98edf
mohammad shariful <shariful24jan@gmail.com>
2024-10-21 14:26:08 +0800

    s

---
+++ problem2.js
@@ -0,0 +1,18 @@
+let number = 8;
+let sum = 0;
+for(let i = 0; i <= 8; i=i+2){
+    if(sum = sum + i){
+    }
+}
+console.log(sum);
+
+
+let number1 = 8;
+ sum = 0;
+for(let i = 1; i <= number1; i++){
+    if(i % 2 === 0){
+    sum = sum + i;
+    }
+}
+console.log(sum);
+
